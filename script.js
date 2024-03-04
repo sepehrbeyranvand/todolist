680d5504cdb70bbb001696513340334a3f17f2e4
sepehrbeyranvand <sepehr7797@gmail.com>
2024-03-05 00:31:41 +0330

    choose color and add jquery

---
+++ script.js
@@ -0,0 +1,13 @@
+//change the default color value
+$('input[type="color"]').val('#2f9aba');
+
+$('button').click(function(){
+    let Value = $('input').val();
+    let theColor = $('input[type="color"]').val();
+
+    $('.container-fluid').append('<div style="background-color:' + theColor + '" class="alert alert-dismissible d-flex fade show">' +
+    '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+    '<h1>' + Value + '</h1>' +
+    '</div>');
+    $('input').val('');
+})
