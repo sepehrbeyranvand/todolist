9971f5207d3bcded0bdb775622b703eeaf27f0e5
sepehrbeyranvand <sepehr7797@gmail.com>
2024-03-05 21:24:22 +0330

    First Version

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $('#taskSend').click(function(){
     let theColor = $('input[type="color"]').val();
     let selectUser =  $('select').val();
 
-    $('.container-fluid').append('<div style="background-color:' + theColor + '" class="alert alert-dismissible d-flex fade show">' +
+    $('.container-fluid').append('<div style="background-color:' + theColor + '" class="alert alert-dismissible w-75 d-flex fade show">' +
     '<button class="btn-close" data-bs-dismiss="alert"></button>' +
     '<div class="d-flex flex-column">'+
     '<h1>' + Value + '</h1>' +
