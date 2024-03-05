8cb776bcfc5314f8979e1f732baad29b8fa2c256
sepehrbeyranvand <sepehr7797@gmail.com>
2024-03-05 18:44:27 +0330

    ADD select and some changes in styles

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //change the default color value
 $('input[type="color"]').val('#2f9aba');
 
-$('taskSend').click(function(){
+$('#taskSend').click(function(){
     let Value = $('input').val();
     let theColor = $('input[type="color"]').val();
+    let selectUser =  $('select').val();
 
     $('.container-fluid').append('<div style="background-color:' + theColor + '" class="alert alert-dismissible d-flex fade show">' +
     '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+    '<div class="d-flex flex-column">'+
     '<h1>' + Value + '</h1>' +
+    '<p class="text-sm text-muted"> '+ selectUser + '</p>'+
+    '</div>'+
     '</div>');
     $('input').val('');
 })
 
 $('#closeModal').click(function(){
-    $('.modal').modal('hide');
+    $('#modalHelp').modal('hide');
 })
