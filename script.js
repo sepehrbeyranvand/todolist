7e5f9d38771ce27618128641cb430ac4beb92558
sepehrbeyranvand <sepehr7797@gmail.com>
2024-03-08 11:17:14 +0330

    The localstorage and deleting button WORKS

--- script.js
+++ script.js
@@ -21,6 +21,62 @@
 //     $('input').val('');
 // })
 
+// $(document).ready(function() {
+//   // Your existing code
+//   $('input[type="color"]').val('#2f9aba');
+
+//   $('#taskSend').click(function() {
+//     let Value = $('input').val();
+//     let theColor = $('input[type="color"]').val();
+//     let Activity = $('#Activity').val();
+//     let DateTime = $('input[type="datetime-local"]').val();
+//     var datetime = new Date();
+//     var now = datetime.toLocaleString();
+
+//     // Append the alert to the container
+//     $('.container-fluid').prepend('<div style="background-color:' + theColor + '" class="alert alert-dismissible w-75 d-flex fade show">' +
+//       '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+//       '<div class="d-flex flex-column">' +
+//       '<h3>' + Value + '</h3>' +
+//       '<p class="text-sm text-muted">' + Activity + '</p>' +
+//       '<p>' + now + '</p>' +
+//       '</div>' +
+//       '</div>');
+
+//     // Clear the input fields
+//     $('input').val('');
+
+//     // Store the alert data in localStorage
+//     var alertData = {
+//       value: Value,
+//       color: theColor,
+//       activity: Activity,
+//       datetime: now
+//     };
+//     localStorage.setItem('alert', JSON.stringify(alertData));
+//   });
+
+//   // Retrieve and display stored alerts when the page loads
+//   function displayStoredAlerts() {
+//     var storedAlert = localStorage.getItem('alert');
+//     if (storedAlert) {
+//       var alertData = JSON.parse(storedAlert);
+//       $('.container-fluid').prepend('<div style="background-color:' + alertData.color + '" class="alert alert-dismissible w-75 d-flex fade show">' +
+//         '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+//         '<div class="d-flex flex-column">' +
+//         '<h3>' + alertData.value + '</h3>' +
+//         '<p class="text-sm text-muted">' + alertData.activity + '</p>' +
+//         '<p>' + alertData.datetime + '</p>' +
+//         '</div>' +
+//         '</div>');
+//     }
+//   }
+//   displayStoredAlerts();
+// });
+
+
+
+
 $(document).ready(function() {
   // Your existing code
   $('input[type="color"]').val('#2f9aba');
@@ -34,7 +90,7 @@ $(document).ready(function() {
     var now = datetime.toLocaleString();
 
     // Append the alert to the container
-    $('.container-fluid').prepend('<div style="background-color:' + theColor + '" class="alert alert-dismissible w-75 d-flex fade show">' +
+    $('.container-fluid').append('<div style="background-color:' + theColor + '" class="alert alert-dismissible w-75 d-flex fade show">' +
       '<button class="btn-close" data-bs-dismiss="alert"></button>' +
       '<div class="d-flex flex-column">' +
       '<h3>' + Value + '</h3>' +
@@ -53,25 +109,42 @@ $(document).ready(function() {
       activity: Activity,
       datetime: now
     };
-    localStorage.setItem('alert', JSON.stringify(alertData));
+    var alertsJSON = localStorage.getItem('alerts');
+    if (alertsJSON) {
+      alertsJSON = JSON.parse(alertsJSON);
+      alertsJSON.push(alertData);
+    } else {
+      alertsJSON = [alertData];
+    }
+    localStorage.setItem('alerts', JSON.stringify(alertsJSON));
   });
 
   // Retrieve and display stored alerts when the page loads
   function displayStoredAlerts() {
-    var storedAlert = localStorage.getItem('alert');
-    if (storedAlert) {
-      var alertData = JSON.parse(storedAlert);
-      $('.container-fluid').prepend('<div style="background-color:' + alertData.color + '" class="alert alert-dismissible w-75 d-flex fade show">' +
-        '<button class="btn-close" data-bs-dismiss="alert"></button>' +
-        '<div class="d-flex flex-column">' +
-        '<h3>' + alertData.value + '</h3>' +
-        '<p class="text-sm text-muted">' + alertData.activity + '</p>' +
-        '<p>' + alertData.datetime + '</p>' +
-        '</div>' +
-        '</div>');
+    var alertsJSON = localStorage.getItem('alerts');
+    if (alertsJSON) {
+      var alertsArray = JSON.parse(alertsJSON);
+
+      // Iterate through the alerts array and display each alert in the container
+      alertsArray.forEach(function(alertData) {
+        $('.container-fluid').append('<div style="background-color:' + alertData.color + '" class="alert alert-dismissible w-75 d-flex fade show">' +
+          '<button class="btn-close" data-bs-dismiss="alert"></button>' +
+          '<div class="d-flex flex-column">' +
+          '<h3>' + alertData.value + '</h3>' +
+          '<p class="text-sm text-muted">' + alertData.activity + '</p>' +
+          '<p>' + alertData.datetime + '</p>' +
+          '</div>' +
+          '</div>');
+      });
     }
   }
   displayStoredAlerts();
+
+  // Add click event listener to delete all alerts
+  $('#removeAllAlerts').click(function() {
+    localStorage.clear();
+    $('.container-fluid .alert').remove();
+  });
 });
 
 
